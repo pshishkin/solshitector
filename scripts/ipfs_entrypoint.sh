17895dee8ef1caa428a4bcdb070436d3525e2f48
pavel <pshishkin@gmail.com>
2025-04-06 17:22:26 +0300

    ipfs added v2

--- scripts/ipfs_entrypoint.sh
+++ scripts/ipfs_entrypoint.sh
@@ -11,6 +11,15 @@ else
   echo "IPFS repository already initialized."
 fi
 
+# Configure API to listen on all interfaces
+echo "Configuring IPFS API and Gateway..."
+ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
+ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
+
+# Configure CORS headers
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST", "GET"]'
+
 # Start the IPFS daemon
 echo "Starting IPFS daemon..."
 exec ipfs daemon --migrate=true
