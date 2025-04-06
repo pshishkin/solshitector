ab093bd6060ba2ad879fbb0891656a1c90e0e8d0
pavel <pshishkin@gmail.com>
2025-04-06 17:38:08 +0300

    local ipfs is working now

--- scripts/ipfs_entrypoint.sh
+++ scripts/ipfs_entrypoint.sh
@@ -16,10 +16,23 @@ echo "Configuring IPFS API and Gateway..."
 ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
 ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
 
-# Configure CORS headers
+# Configure CORS headers for API
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
-ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST", "GET"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST", "GET", "OPTIONS"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Headers '["X-Requested-With", "Range", "User-Agent", "X-Requested-With", "Origin", "Content-Type"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Expose-Headers '["X-Stream-Output", "X-Chunked-Output", "X-Content-Length"]'
 
-# Start the IPFS daemon
+# Configure the Gateway
+ipfs config --json Gateway.APICommands '["get", "dag/get", "cat", "ls", "refs", "refs/local", "resolve", "files/read", "version"]'
+
+# Set localhost as a specifically configured gateway with subdomains disabled
+ipfs config --json Gateway.PublicGateways '{
+  "localhost": {
+    "Paths": ["/ipfs", "/ipns"],
+    "UseSubdomains": false
+  }
+}'
+
+# Start the IPFS daemon 
 echo "Starting IPFS daemon..."
 exec ipfs daemon --migrate=true
